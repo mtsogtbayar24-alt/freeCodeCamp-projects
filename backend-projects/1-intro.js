7018437ec4b29268e76bfac4ecb718ab0aa66e01
Tsogtbayar <mtsogtbayar24@gmail.com>
2026-02-23 22:47:54 +0800

    fccFullJSVideo

---
+++ backend-projects/1-intro.js
@@ -0,0 +1,9 @@
+const amount = 10;
+
+if (amount < 12) {
+  console.log('Small number');
+} else {
+  console.log('Large number');
+}
+
+console.log(`Hey it's my first node app!!!`);
---
+++ backend-projects/index.js
@@ -0,0 +1,119 @@
+// document.getElementById("count").innerText = 5;
+// let firstBatch = 5;
+// let secondBatch = 7;
+// let count = firstBatch + secondBatch;
+// console.log(count);
+
+
+// let myAge = 20;
+// let humanDogRatio = 7;
+// let myDogAge = myAge * humanDogRatio;
+// console.log(myDogAge);
+
+// console.log(myAge);
+
+// let count = 5;
+// count = count + 1;
+// console.log(count);
+
+// let bonusPoints = 50;
+// console.log(bonusPoints);
+// bonusPoints = bonusPoints + 50;
+// console.log(bonusPoints);
+// bonusPoints = bonusPoints - 75;
+// console.log(bonusPoints);
+// bonusPoints = bonusPoints + 45;
+
+// console.log(bonusPoints);
+
+//subway counter app
+//lesson 8, 14
+
+// initialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
+// change the count-el in the HTML to reflect the new count
+
+// function increment() {
+//   count = count + 1;
+//   console.log(count);}
+// document.getElementById("count-el").innerText = count;
+
+
+// function increment() {
+//     console.log('The button was clicked');
+// }
+
+// lesson 9
+// function countDown() {
+//     console.log(5);
+//     console.log(4);
+//     console.log(3);
+//     console.log(2);
+//     console.log(1);
+// }
+
+// // setting up the the race
+
+// countDown();
+// // GO
+// // Players are running the race
+// // Race is finished
+
+// //Get ready for new race
+// countDown();
+
+// lesson 10
+
+// function fortyTwo() {
+//     console.log(42);
+// }
+
+// fortyTwo();
+
+// lesson 11
+// let lap1 = 34;
+// let lap2 = 33;
+// let lap3 = 36;
+
+// function totalLapTime() {
+//     //let totalTime = lap1 + lap2 + lap3;
+//     console.log(lap1 + lap2 + lap3);
+// }
+
+// totalLapTime();
+
+// lesson 12
+// let lapsCompleted = 0;
+
+// function increment() {
+//     lapsCompleted = lapsCompleted + 1;
+//     //console.log(lapsCompleted);
+// } 
+// increment();
+// increment();
+// increment();
+
+// console.log(lapsCompleted);
+
+// lesson 13
+
+// camelCase
+
+// let countEl = document.getElementById("count-el");
+
+// console.log(countEl);
+ 
+// let count = 0;
+
+// function increment() {
+//     count = count + 1;
+//     countEl.innerText = count;
+// }
+
+// lesson 14
+function save() {
+    console.log(count);
+}
+
+
